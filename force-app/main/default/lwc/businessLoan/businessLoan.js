0e87bb79697d0323b35301a36ac7b83be4de8400
Aditi <aditisbadkur@gmail.com>
2025-06-26 15:57:37 +0530

    get record details for sections works lesgoo

--- force-app/main/default/lwc/businessLoan/businessLoan.js
+++ force-app/main/default/lwc/businessLoan/businessLoan.js
@@ -1,3 +1,41 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
+import getData from '@salesforce/apex/getApplicantData.getData';
 
-export default class BusinessLoan extends LightningElement {}
+export default class BusinessLoan extends LightningElement {
+    @api recordId;
+    isLoading = true;
+    applicationData = {};
+    error;
+    
+    @wire(getData, { recordId: '$recordId' })
+    wiredApplication({ error, data }) {
+        this.isLoading = false;
+        
+        if (data) {
+            this.applicationData = data.length > 0 ? {
+                ...data[0], 
+                formattedLoanAmount: this.formatCurrency(data[0].Loan_Amount__c),
+                formattedIncome: this.formatCurrency(data[0].Annual_Income__c)
+            } : {};
+            this.error = undefined;
+        } else if (error) {
+            this.error = error.body ? error.body.message : 'Unknown error';
+            this.applicationData = {};
+        }
+    }
+    
+    formatCurrency(value) {
+        return new Intl.NumberFormat('en-IN', { 
+            style: 'currency', 
+            currency: 'INR' 
+        }).format(value || 0);
+    }
+    
+    get hasApplications() {
+        return this.applicationData && this.applicationData.Id;
+    }
+    
+    get loanType() {
+        return this.applicationData?.Loan_Type__c || 'Not specified';
+    }
+}
